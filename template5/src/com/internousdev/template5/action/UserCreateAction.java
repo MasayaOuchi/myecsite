9639051a96ca9bfcbe268afd21023b3e8b8d07af
MasayaOuchi <mac.mac.jp5@icloud.com>
2018-11-29 17:09:04 +0900

    11/29

---
+++ template5/src/com/internousdev/template5/action/UserCreateAction.java
@@ -0,0 +1,13 @@
+package com.internousdev.template5.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserCreateAction extends ActionSupport {
+		public class UserCreationAction extends ActionSupport {
+			public String execute() {
+				return SUCCESS;
+			}
+		}
+
+
+}
